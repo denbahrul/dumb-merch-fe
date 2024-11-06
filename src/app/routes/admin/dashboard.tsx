571bf33bb3873076bcaf05ec61407b74c3005926
denbahrul <ulum0326@gmail.com>
2024-11-06 13:46:38 +0700

    fix : admin dashboard

--- src/app/routes/admin/dashboard.tsx
+++ src/app/routes/admin/dashboard.tsx
@@ -2,9 +2,9 @@ import Statistic from "@/features/admin-dashboard/components/statistic";
 
 export default function AdminDashboard() {
   return (
-    <div className="p-8">
+    <div className="">
       <div className="mb-2 flex items-center justify-between">
-        <h1 className="my-8 text-2xl font-bold">Dashboard</h1>
+        <h1 className="my-8 px-8 pt-8 text-2xl font-bold">Dashboard</h1>
       </div>
       <Statistic />
     </div>
